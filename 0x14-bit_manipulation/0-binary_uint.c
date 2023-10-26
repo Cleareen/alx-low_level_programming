55f1a5d99b9f16361fe801e53cabb427b2e69195
EDDIE MKANSI <51905275+Cleareen@users.noreply.github.com>
2023-10-26 20:29:56 +0200

    Update 0-binary_uint.c

--- 0x14-bit_manipulation/0-binary_uint.c
+++ 0x14-bit_manipulation/0-binary_uint.c
@@ -1,30 +1,54 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * binary_to_uint - function converts a binary number to an unsigned int.
- * @b: pointer to a string containing a binary number
- *
- * Return: unsigned int with decimal value of binsry number, or 0 if error
+ * binary_to_uint - binary can be converted through unsigned int
+ * @b: A string that takes binary number or numbers
+ * Return: A numbers that are coverted
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	int a;
-	unsigned int num;
+        int alx;
+        unsigned int abraham = 0;
 
-	num = 0;
-	if (!b)
-		return (0);
-	for (a = 0; b[a] != '\0'; a++)
-	{
-		if (b[a] != '0' && b[a] != '1')
-			return (0);
-	}
-	for (a = 0; b[a] != '\0'; a++)
-	{
-		num <<= 1;
-		if (b[a] == '1')
-			num += 1;
-	}
-	return (num);
+        if (!b)
+                return (0);
+        for (alx = 0; b[alx]; alx++)
+        {
+                if (b[alx] < '0' || b[alx] > '1')
+                        return (0);
+                abraham = 2 * abraham + (b[alx] - '0');
+        }
+        return (abraham);
 }
 
+
+===============1-print_binary.c=========
+
+#include "main.h"
+
+/**
+ * print_binary - equivalent of a decimal number that prints a binary
+ * @n: the amount or number binary to be printed
+ */
+
+void print_binary(unsigned long int n)
+{
+        int leona, Sis_hoffmann = 0;
+        unsigned long int new;
+
+        for (leona = 63; leona >= 0; leona--)
+        {
+                new = n >> leona;
+                if (new & 1)
+                {
+                        _putchar('1');
+                        Sis_hoffmann++;
+                }
+                else if (new)
+                        _putchar('0');
+        }
+        if (!new)
+                _putchar('0');
+}
